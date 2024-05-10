b9d51b35300352e467df3e06f8606209579e4a6e
Rajat Sharma <lunasunkaiser@gmail.com>
2024-05-10 18:24:05 +0530

    add del fn

--- src/lib.rs
+++ src/lib.rs
@@ -4,29 +4,43 @@ pgrx::pg_module_magic!();
 
 extension_sql!(
     r#"
-CREATE TABLE kv (
+CREATE TABLE pgkv (
     id text not null primary key,
     value json not null
 );
 "#,
-    name = "create_kv_table",
+    name = "create_pgkv_table",
 );
 
 #[pg_extern]
 fn get(key: &str) -> Result<Option<pgrx::Json>, spi::Error> {
-    Spi::get_one_with_args(
-        "SELECT value FROM kv WHERE id = $1",
+    let result = Spi::get_one_with_args(
+        "SELECT value FROM pgkv WHERE id = $1",
         vec![(PgBuiltInOids::TEXTOID.oid(), key.into_datum())],
-    )
+    );
+
+    if let Err(_) = result {
+        return Ok(None);
+    }
+
+    result
 }
 
 #[pg_extern]
 fn set(key: &str, value: &str) -> Result<Option<String>, spi::Error> {
     Spi::get_one_with_args(
-        "INSERT INTO kv (id, value) VALUES ($1, $2) RETURNING id",
+        "INSERT INTO pgkv (id, value) VALUES ($1, $2) RETURNING id",
         vec![
             (PgBuiltInOids::TEXTOID.oid(), key.into_datum()),
             (PgBuiltInOids::JSONOID.oid(), value.into_datum()),
         ],
+    )
+}
+
+#[pg_extern]
+fn del(key: &str) -> Result<Option<String>, spi::Error> {
+    Spi::get_one_with_args(
+        "DELETE FROM pgkv WHERE id = $1 RETURNING id",
+        vec![(PgBuiltInOids::TEXTOID.oid(), key.into_datum())],
     )
 }
